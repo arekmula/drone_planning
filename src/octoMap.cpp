ed361f0cc2788e1d3dd932911b1795ee96ef417d
arekmula <arekmula97@gmail.com>
2020-04-25 12:57:09 +0200

    Changed Callback function

--- src/octoMap.cpp
+++ src/octoMap.cpp
@@ -1,18 +1,48 @@
+
+// ROS
 #include <ros/ros.h>
-#include <drone_planning/conversions.h>
 #include <octomap/octomap.h>
 #include <octomap_msgs/Octomap.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <nav_msgs/Path.h>
+
+#include <moveit/ompl_interface/ompl_interface.h>
+#include <ompl/geometric/planners/rrt/RRTConnect.h>
+#include <ompl/geometric/planners/prm/LazyPRMstar.h>
+#include <ompl/base/spaces/RealVectorStateSpace.h>
+
+// Eigen
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Geometry>
+
+// Octomap conversions
+#include <drone_planning/conversions.h>
+
+// Boost
+#include <boost/thread.hpp>
+
+//standard
+#include <mutex>
+#include <iostream>
+#include <thread>
+#include <fstream>
+#include <iomanip>
 
 
-void octomap_listener(const octomap_msgs::Octomap &octMap)
+
+octomap_msgs::Octomap globalMap;
+
+
+void octomapCallback(const octomap_msgs::OctomapPtr& octMap)
 {
-//    for(int i=0; i < octMap.data.size(); i++){
-//        std::cout << int(octMap.data[i]) << "\n";
-//    }
-    std::cout << "octMap Resolution: " << octMap.resolution << "\n";
-    std::cout << "octMap Binary: " << octMap.binary <<"\n";
-    std::cout << "octMap id: " << octMap.id << "\n";
+
+    globalMap = *octMap;
+    std::cout << "globalMap Resolution: " << globalMap.resolution << "\n";
+    std::cout << "globalMap Binary: " << globalMap.binary <<"\n";
+    std::cout << "globalMap id: " << globalMap.id << "\n";
+    //    for(int i=0; i < globalMap.data.size(); i++){
+    //        std::cout << int(globalMap.data[i]) << "\n";
+    //    }
 }
 
 void octomapCloud_listener(const sensor_msgs::PointCloud2 &cloud){
@@ -30,7 +60,7 @@ int main(int argc, char **argv)
 
 
   // GPS Fix subscribers
-  ros::Subscriber octomapFull_sub = node.subscribe("/octomap_full", 10, octomap_listener);
+  ros::Subscriber octomapFull_sub = node.subscribe("/octomap_full", 10, octomapCallback);
   ros::Subscriber octmapPointCloud_sub = node.subscribe("/octomap_point_cloud_centers", 10, octomapCloud_listener);
 
 
