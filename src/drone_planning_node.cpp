feacca16682f4958722caacd686049c55091306b
Maciej <maciej.skwara2@gmail.com>
2020-05-11 00:52:16 +0200

    update2

--- src/drone_planning_node.cpp
+++ src/drone_planning_node.cpp
@@ -1,57 +1,14 @@
-#pragma once
+// ROS
+#include <ros/ros.h>
+#include "../include/drone_planning/drone_planning.hpp"
 
-#include <sensor_msgs/point_cloud_conversion.h>
-#include <boost/foreach.hpp>
-#include <geometry_msgs/Pose.h>
-#include <geometry_msgs/PoseWithCovariance.h>
-#include <geometry_msgs/Point.h>
-#include <geometry_msgs/Quaternion.h>
-#include <geometry_msgs/Twist.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <octomap_msgs/Octomap.h>
-#include <octomap/octomap.h>
-#include <octomap/AbstractOcTree.h>
-#include <octomap/ColorOcTree.h>
-#include <sensor_msgs/PointCloud2.h>
-#include <nav_msgs/Path.h>
-#include <nav_msgs/OccupancyGrid.h>
-#include <ros/package.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <visualization_msgs/Marker.h>
 
-
-#include <fcl/config.h>
-#include <fcl/geometry/octree/octree.h>
-#include <fcl/common/types.h>
-#include <fcl/octree.h>
-#include <fcl/data_types.h>
-#include <fcl/math/vec_3f.h>
-#include <fcl/math/math_details.h>
-
-#include <fcl/narrowphase/collision.h>
-#include <fcl/geometry/collision_geometry.h>
-
-
-
-#include <cmath>
-#include <limits>
-
-#include <octomap_ros/conversions.h>
-#include <grid_map_octomap/GridMapOctomapConverter.hpp>
-#include <grid_map_octomap/grid_map_octomap.hpp>
-#include <moveit/ompl_interface/ompl_interface.h>
-
-#include <ompl/geometric/planners/rrt/RRTConnect.h>
-#include <ompl/geometric/planners/prm/LazyPRMstar.h>
-#include <ompl/base/spaces/RealVectorStateSpace.h>
-#include <ompl/base/spaces/RealVectorBounds.h>
-#include <ompl/base/spaces/SE3StateSpace.h>
-#include <moveit/ompl_interface/ompl_interface.h>
 
 // Eigen
 #include <eigen3/Eigen/Core>
 #include <eigen3/Eigen/Geometry>
-#include <eigen3/Eigen/Eigen>
-#include <eigen3/Eigen/Dense>
-
 
 // Octomap conversions
 #include <drone_planning/conversions.h>
@@ -67,61 +24,89 @@
 #include <iomanip>
 
 
+octomap_msgs::Octomap globalOctoMap;
+sensor_msgs::PointCloud2 globalPointCloud;
+nav_msgs::OccupancyGrid globalOccupancyMap;
+visualization_msgs::MarkerArray markerArray;
+
+void octomapCallback(const octomap_msgs::OctomapPtr& msg)
+{
+  /*!
+  * Octomap callback function
+  * commented lines are examples of getting to octomap data
+  */
+    globalOctoMap = *msg;
+
+}
+
+void pointCloudCallback(const sensor_msgs::PointCloud2Ptr& cloud){
+
+    /*!
+    * PointCloud2 callback function
+    * commented lines are examples of getting to PointCloud data
+    */
+    globalPointCloud = *cloud;
+
+}
+
+void markerArrayCallback(const visualization_msgs::MarkerArrayPtr& mArray)
+{
+    /*!
+    * markerArray callback function
+    * commented lines are examples of getting to markerArray data
+    */
+    markerArray = *mArray;
 
 
+}
 
-namespace drone_planning{
+void occupancyMapCallback(const nav_msgs::OccupancyGridPtr& oMap)
+{
+    /*!
+    * occupancy map callback function
+    * commented lines are examples of getting to occupancy data
+    */
 
-    class Planner3D
-    {
-    public:
-        /*!
-         * Constructor
-         * @param nodeHandle the ros node Handle.
-         */
-        Planner3D(ros::NodeHandle& _nodeHandle);
+    globalOccupancyMap = *oMap;
+}
 
-        /*!
-        * Destructor
-        */
-        virtual ~Planner3D();
+int main(int argc, char **argv)
+{
+  /// init ROS node
+  ros::init(argc, argv, "drone_planner");
 
-        /*!
-         * plan path
-         * @param octomapMsg - Octomap message of enviroment
-         */
-        nav_msgs::Path planPath(const octomap_msgs::Octomap& octomapMsg);
-        /// robot mesh points
+  ///  create node handler
+  ros::NodeHandle node("~");
+  drone_planning::Planner3D planner_(node);
 
+  /// setup ROS lopp rate
+  ros::Rate loop_rate(1);
 
+  /// subscribers to full octomap, octomap point cloud and markerArray
+  ros::Subscriber octomapFull_sub = node.subscribe("/octomap_full", 10, octomapCallback);
+  ros::Subscriber octmapPointCloud_sub = node.subscribe("/octomap_point_cloud_centers", 10, pointCloudCallback); // Wspolrzedne zajetych voxeli
+  ros::Subscriber occupied_cells_vis_array_sub = node.subscribe("/occupied_cells_vis_array", 10, markerArrayCallback);
+  ros::Subscriber occupancyMap_sub = node.subscribe("/projected_map", 10, occupancyMapCallback);
 
-    private:
-        /// node handle
-        ros::NodeHandle& nodeHandle;
-
-        /// problem dimension
-        int dim;
-
-        /// max step Length
-        double maxStepLength;
-
-        /// bounds for all dimensions
-        std::shared_ptr<ompl::base::RealVectorBounds> bounds;
-
-        /// starting and goal position
-        std::shared_ptr<ompl::base::ScopedState<>> start;
-        std::shared_ptr<ompl::base::ScopedState<>> goal;
-
-        /// space of problem
-        std::shared_ptr<ompl::base::SE3StateSpace> space;
-
-        /// configure
-        void configure(void);
-
-        /// extract path function
-        nav_msgs::Path extractPath(ompl::base::ProblemDefinition* pdef);
+  /// path Publisher
+  ros::Publisher path_pub = node.advertise<nav_msgs::Path>("my_path",1000);
 
 
-    };
+  while (ros::ok()){
+      nav_msgs::Path plannedPath;
 
+      /// calculating path
+      if(globalOctoMap.data.size()>0) /// make sure that node has subsribed to octomap data
+      {
+          plannedPath = planner_.planPath(globalOctoMap);
+      }
+
+      /// publishing path
+      path_pub.publish(plannedPath);
+
+      ros::spinOnce();
+      loop_rate.sleep();
+}
+    return 0;
+//  ros::spin();
 }
