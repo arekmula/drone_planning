2fa4d03264ca066252c36ae1a1bd20ea6fd148f0
Maciej <maciej.skwara2@gmail.com>
2020-05-27 01:04:57 +0200

    Add drone flying

--- src/drone_planning_node.cpp
+++ src/drone_planning_node.cpp
@@ -23,6 +23,8 @@
 #include <fstream>
 #include <iomanip>
 
+#include <nav_msgs/Odometry.h>
+#include <visualization_msgs/Marker.h>
 
 octomap_msgs::Octomap globalOctoMap;
 sensor_msgs::PointCloud2 globalPointCloud;
@@ -81,8 +83,12 @@ int main(int argc, char **argv)
 
   ros::init(argc, argv, "robot_tf_publisher");
   ros::NodeHandle n;
+  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
   ros::Rate r(100);
   tf::TransformBroadcaster broadcaster;
+
+
+
 
   /// setup ROS lopp rate
   ros::Rate loop_rate(1);
@@ -95,10 +101,30 @@ int main(int argc, char **argv)
 
   /// path Publisher
   ros::Publisher path_pub = node.advertise<nav_msgs::Path>("my_path",1000);
+  ros::Publisher vis_pub = node.advertise<visualization_msgs::Marker>( "visualization_marker", 1000 );
 
 
   while (ros::ok()){
       nav_msgs::Path plannedPath;
+
+      /// Visualization marker
+      visualization_msgs::Marker marker;
+      marker.header.frame_id = "tf";
+      marker.header.stamp = ros::Time();
+      marker.ns = "my_namespace";
+      marker.id = 0;
+      marker.type = visualization_msgs::Marker::MESH_RESOURCE;
+      marker.action = visualization_msgs::Marker::ADD;
+      marker.scale.x = 1.0;
+      marker.scale.y = 1.0;
+      marker.scale.z = 1.0;
+      marker.color.a = 1.0; // Don't forget to set the alpha!
+      marker.color.r = 1.0;
+      marker.color.g = 1.0;
+      marker.color.b = 0.0;
+      marker.mesh_resource = "package://drone_planning/meshes/quadrotor/quadrotor_2.dae";
+
+
 
       /// calculating path
       if(globalOctoMap.data.size()>0) /// make sure that node has subsribed to octomap data
@@ -111,6 +137,8 @@ int main(int argc, char **argv)
 
       /// publishing path
       path_pub.publish(plannedPath);
+
+
 
       /// TFs
       for (int i = 0 ; i < plannedPath.poses.size() ; i++)
@@ -131,8 +159,48 @@ int main(int argc, char **argv)
                                           plannedPath.poses[i].pose.position.y - 0.5,
                                           plannedPath.poses[i].pose.position.z - 0.3)),
                           ros::Time::now(), "drone","tf"));
-          
+          geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(0);
+          nav_msgs::Odometry odom;
+          odom.header.stamp = ros::Time::now();
+          odom.header.frame_id = "odom";
+
+          //set the position
+          odom.pose.pose.position.x = 0;
+          odom.pose.pose.position.y = 0;
+          odom.pose.pose.position.z = 0.0;
+          odom.pose.pose.orientation = odom_quat;
+
+          //set the velocity
+          odom.child_frame_id = "drone";
+          odom.twist.twist.linear.x = 0;
+          odom.twist.twist.linear.y = 0;
+          odom.twist.twist.angular.z = 0;
+
+          //publish the message
+          odom_pub.publish(odom);
+
+          /// Drone marek pose
+          marker.pose.orientation.x = plannedPath.poses[i].pose.position.x;
+          marker.pose.orientation.y = plannedPath.poses[i].pose.position.y;
+          marker.pose.orientation.z = plannedPath.poses[i].pose.position.z;
+
+          /// Drone marker orientation
+          marker.pose.orientation.x = 0.0;
+          marker.pose.orientation.y = 0.0;
+          marker.pose.orientation.z = 0.0;
+          marker.pose.orientation.w = 1.0;
+
+          /// ADD DRONE
+          marker.action = visualization_msgs::Marker::ADD;
+
+          /// publishing marker
+          vis_pub.publish(marker);
+
           ros::Duration(1.0).sleep();
+
+          /// DELETE DRONE
+          marker.action = visualization_msgs::Marker::DELETE;
+          
       }
 
 
