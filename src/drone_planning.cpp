181b5a102041e099a10ac35a53884bf979f45259
arekmula <arekmula97@gmail.com>
2020-05-27 15:32:56 +0200

    Read rotation of drone

--- src/drone_planning.cpp
+++ src/drone_planning.cpp
@@ -188,15 +188,17 @@ nav_msgs::Path Planner3D::extractPath(ompl::base::ProblemDefinition *pdef)
         /// get coordinates of robot
         const auto *coordinates = state->as<ompl::base::CompoundState>()->
                 as<ompl::base::RealVectorStateSpace::StateType>(0);
+        const auto *rotation = state->as<ompl::base::CompoundState>()->
+                as<ompl::base::SO3StateSpace::StateType>(1);
         /// fill in the ROS PoseStamped Structure
         geometry_msgs::PoseStamped poseMsg;
         poseMsg.pose.position.x = coordinates->values[0];
         poseMsg.pose.position.y = coordinates->values[1];
         poseMsg.pose.position.z = coordinates->values[2];
-        poseMsg.pose.orientation.w = 1.0; // in the future this might also be calculated from coordinates, probably [3,4,5,6]
-        poseMsg.pose.orientation.x = 0.0;
-        poseMsg.pose.orientation.y = 0.0;
-        poseMsg.pose.orientation.z = 0.0;
+        poseMsg.pose.orientation.w = rotation->w;
+        poseMsg.pose.orientation.x = rotation->x;
+        poseMsg.pose.orientation.y = rotation->y;
+        poseMsg.pose.orientation.z = rotation->z;
         poseMsg.header.frame_id = "/odom";
         poseMsg.header.stamp = ros::Time::now();
         /// add poseStamped to the path
